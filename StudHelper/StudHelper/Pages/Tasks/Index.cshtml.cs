c394a7b34ecf8101564e2d1efd12d00d451fc2a7
Yurii Kalichun <yurkok12@gmail.com>
2021-04-29 02:35:18 +0300

    ldfjdsfjn

--- StudHelper/StudHelper/Pages/Tasks/Index.cshtml.cs
+++ StudHelper/StudHelper/Pages/Tasks/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using StudHelper.Models;
 
 namespace StudHelper.Pages.Tasks
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly StudHelper.Data.StudHelperContext _context;
